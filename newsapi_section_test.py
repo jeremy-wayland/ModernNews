0784c33b209132256d34d4ee0486291047d82d6f
dozomatt <dozomatt@gmail.com>
2023-06-26 13:21:10 -0400

    updated organization, defined Brief test

---
+++ newsapi_section_test.py
@@ -0,0 +1,19 @@
+"Interface for generating a Brief section for given NewsAPI results"
+
+from data_loader.news_api import newsapi_load_content
+from src.content_summary import get_news_summary
+
+# User search
+# user_search = "women health wellness"
+user_search = "NVDA"
+
+# Get NewsAPI content
+result = newsapi_load_content(user_search)
+
+# Summarize NewsAPI content
+brief_content = get_news_summary(result, user_search)
+
+print(brief_content)
+
+# print(result)
+
---
+++ src/content_summary.py
@@ -0,0 +1,59 @@
+"Interface for summarizing news content"
+
+import argparse
+import sys
+import os
+import pandas as pd
+from dotenv import load_dotenv
+from langchain import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains.summarize import load_summarize_chain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+# Define key
+load_dotenv()
+openai_key = os.getenv("OPENAI_KEY")
+
+# Function to compile all content into one for OpenAI to pick out topics
+def column_to_string(df):
+    first_column = df.iloc[:, 0]  # Extract the first column
+    aggregated_string = ', '.join(first_column.astype(str))  # Convert values to strings and join them
+    return aggregated_string
+
+# Function to summarize article content
+def get_news_summary(df_news, user_search):
+
+    # Get news for company
+    # try:
+    content = column_to_string(df_news)
+
+    print('content length:', len(content))
+
+    # Define OpenAI client
+    llm = ChatOpenAI(temperature=0.5, model="gpt-3.5-turbo", openai_api_key=openai_key)
+
+    # Reduce content to chunks
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 4000,
+        chunk_overlap  = 100
+        )
+    docs = text_splitter.create_documents([content])
+
+    print('docs length:', len(docs))
+
+    # Define separate prompts to handle docs indivdually and collectively 
+    prompt_template = f"""Write a concise summary of the following news only as it pertains to {user_search}. If not at all related to that topic, just output an empty string: """ + """{text}""" 
+    combined_prompt_template = f"""Write a concise and cohesive editorial piece in 4-6 sentences summarizing the following news, using a mix of journalistic and conversational language, written in Chicago style, and avoidant of redundancies.""" + """{text}""" 
+    
+    prompt = PromptTemplate(template=prompt_template, input_variables=["text"])
+    combined_prompt = PromptTemplate(template=combined_prompt_template, input_variables=["text"])
+
+    # Init Summarization Chain to summarize content chunks
+    chain = load_summarize_chain(llm, chain_type="map_reduce", map_prompt=prompt, combine_prompt=combined_prompt)
+
+    reduced = chain.run(docs)
+
+    return reduced
+    
+    # except:
+    #     return "No News."
